9110fde9a52812c290576e71f291bcd506b845c5
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2022-10-20 15:28:09 +1100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.4',
+  defaultNetwork: 'goerli',
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/48I8EVh9NdEbuAG0oc0UDWHLqMVfuDdM',
+      accounts: ['0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0'],
+    },
+  },
 };
+
